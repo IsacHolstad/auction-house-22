ca649104be0254b986b6748f7d586d64335ce50d
isac holstad <wisac22@gmail.com>
2022-11-23 21:55:02 +0100

    profile page made

--- auction-house/vite.config.js
+++ auction-house/vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
                 home: resolve(__dirname, 'index.html'),
                 login: resolve(__dirname, 'login.html'),
                 signup: resolve(__dirname, 'signup.html'),
-                welcome: resolve(__dirname, 'welcome.html')
+                welcome: resolve(__dirname, 'welcome.html'),
+                profile: resolve(__dirname, 'profile.html')
             }
         }
     }
