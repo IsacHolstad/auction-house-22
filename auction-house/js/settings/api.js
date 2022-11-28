2fdf5ed64b15cf93cca8d11a70195909d3606320
isac holstad <wisac22@gmail.com>
2022-11-28 15:33:21 +0100

    auction listinga api url added to api.js file

--- auction-house/js/settings/api.js
+++ auction-house/js/settings/api.js
@@ -2,11 +2,12 @@ const API_BASE_URL = "https://nf-api.onrender.com/"
 
 const USER_REGISTER_URL = API_BASE_URL + "/api/v1/auction/auth/register"
 const USER_LOGIN = API_BASE_URL + "/api/v1/auction/auth/login"
+const AUCTION_LISTINGS = API_BASE_URL + "/api/v1/auction/listings"
 
 
 export{
     USER_REGISTER_URL,
     API_BASE_URL,
     USER_LOGIN,
-    
+
 }
