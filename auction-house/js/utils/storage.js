ddbd164fdc84649aaf3cdede2eac94e886ee5e15
isac holstad <wisac22@gmail.com>
2022-11-29 15:30:12 +0100

    storage.js for token keys

---
+++ auction-house/js/utils/storage.js
@@ -0,0 +1,49 @@
+const tokenKey = "token";
+const userKey = "user";
+
+function saveToken(token) {
+    console.log("token: ", token)
+    console.log("tokenKey: ", tokenKey)
+    saveToStorage(tokenKey, token);
+}
+
+function getToken() {
+    const value = localStorage.getItem(tokenKey);
+    if (value) {
+        return JSON.parse(value); // convert to js
+    } else {
+        return null
+    }
+}
+
+function saveUser(user) {
+    saveToStorage(userKey, user);
+}
+
+function getUserName() {
+    const user = getFromStorage(userKey);
+    if (userKey) {
+        return user.name
+    } else {
+        return null;
+    }
+}
+
+function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
+}
+
+function getFromStorage(key) {
+    const value = localStorage.getItem(key);
+    if (value) {
+        return JSON.parse(value); // convert to js
+    } else {
+        return []
+    }
+}
+
+function clearStorage() {
+    localStorage.clear();
+}
+
+export {getToken, saveToken, saveUser, getUserName, clearStorage};
