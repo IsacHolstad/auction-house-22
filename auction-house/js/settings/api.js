6fcf81775430fd94e657049bc500d68a2b924e47
isac holstad <wisac22@gmail.com>
2022-11-29 13:40:26 +0100

    added enpoints for auction listings

--- auction-house/js/settings/api.js
+++ auction-house/js/settings/api.js
@@ -3,7 +3,13 @@ const API_BASE_URL = "https://nf-api.onrender.com/"
 const USER_REGISTER_URL = API_BASE_URL + "/api/v1/auction/auth/register"
 const USER_LOGIN = API_BASE_URL + "/api/v1/auction/auth/login"
 const AUCTION_LISTINGS = API_BASE_URL + "/api/v1/auction/listings"
+const POST_AUCTION = API_BASE_URL + "/api/v1/auction/listings"
 
+
+// TODO  update auction = "/api/v1/auction/listings/<id>"
+//TODO delete auction = "/api/v1/auction/listings/<id>
+
+//TODO get single list item use /api/auction/listings/<id>
 
 export{
     USER_REGISTER_URL,
