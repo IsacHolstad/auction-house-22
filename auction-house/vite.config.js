a744ca04f2991623473e5a8de60ed6f74992efd0
isac holstad <wisac22@gmail.com>
2022-11-30 23:10:06 +0100

    spesific auction listings html page made

--- auction-house/vite.config.js
+++ auction-house/vite.config.js
@@ -10,7 +10,8 @@ export default defineConfig({
                 login: resolve(__dirname, 'login.html'),
                 signup: resolve(__dirname, 'signup.html'),
                 welcome: resolve(__dirname, 'welcome.html'),
-                profile: resolve(__dirname, 'profile.html')
+                profile: resolve(__dirname, 'profile.html'),
+                auction_item: resolve(__dirname, 'spesific-listing.html')
             }
         }
     }
