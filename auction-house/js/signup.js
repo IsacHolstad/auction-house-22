258979bb85b3216ba740ab37e518541e64234383
isac holstad <wisac22@gmail.com>
2022-11-30 16:14:17 +0100

    signup validation

--- auction-house/js/signup.js
+++ auction-house/js/signup.js
@@ -27,3 +27,94 @@ const generalErrorMessage = document.querySelector("#general-error-message");
 console.log(signUpForm, firstName, firstNameError, email, emailError, emailErrorNotValid, passwordError, password, confirmPasswordErrorNotMatching, confirmPassword, confirmPasswordError, generalErrorMessage);
 
 
+signUpForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let isFirstName = false;
+    if (firstName.value.trim().length > 0){
+        firstNameError.classList.add("hidden");
+        isFirstName = true;
+    } else {
+        firstNameError.classList.remove("hidden");
+    }
+    let isEmail = false;
+    if (email.value.trim().length > 0) {
+        emailError.classList.add("hidden");
+        isEmail = true;
+
+    } else {
+        emailError.classList.remove("hidden")
+    }
+
+    let isValidEmail = false;
+    if (email.value.trim().length && validationEmail(email.value) === true) {
+        emailErrorNotValid.classList.add("hidden");
+        isValidEmail = true;
+
+    } else if (email.value.trim().length && validateEmail(email.value) !== true) {
+        emailErrorNotValid.classList.remove("hidden")
+    }
+    let isPassword = false;
+    if (password.value.trim().length >= 8) {
+        passwordError.classList.add("hidden")
+        isPassword = true;
+
+    } else {
+        passwordError.classList.remove("hidden")
+    }
+    let isConfirmPassword = false;
+    if (confirmPassword.value.trim().length >= 8) {
+        confirmPassword.classList.add("hidden");
+        isConfirmPassword = true;
+
+    } else {
+        confirmPasswordError.classList.remove("hidden")
+    }
+    let isValidPasswordMatch = false;
+    isValidPasswordMatch = validatePassword(password.value, confirmPassword.value);
+    if (isValidPasswordMatch) {
+        confirmPasswordErrorNotMatching.classList.add("hidden")
+        isValidPasswordMatch = true
+    } else {
+        confirmPasswordErrorNotMatching.classList.remove("hidden")
+    }
+    let isFormValid = isFirstName &&
+        isEmail &&
+        isValidEmail &&
+        isPassword &&
+        isConfirmPassword &&
+        isValidPasswordMatch;
+
+    if (isFormValid) {
+        const userData = {
+            "name": firstName.value,
+            "email": email.value,
+            "password": password.value
+        }
+        const REGISTER_USER_URL_ENDPOINT = USER_REGISTER_URL;
+        (async function signUp() {
+            try {
+                const response = await fetch(REGISTER_USER_URL_ENDPOINT, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(userData)
+                });
+                const data = await response.json();
+                if (response.ok) {
+                    location.replace("/")
+                } else {
+                    generalErrorMessage.innerHTML = `sorry ${data.message}`
+                }
+
+
+            } catch (e) {
+                console.log(e)
+            }
+        })()
+    } else {
+        console.log("validation failed")
+    }
+
+});
