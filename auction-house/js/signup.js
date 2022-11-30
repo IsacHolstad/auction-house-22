ec2940f5d35caa091acab656e38ff636500985a0
isac holstad <wisac22@gmail.com>
2022-11-30 15:52:27 +0100

    all inputs selected

--- auction-house/js/signup.js
+++ auction-house/js/signup.js
@@ -12,11 +12,18 @@ const firstNameError = document.querySelector("#firstNameError");
 
 const email = document.querySelector("#email")
 const emailError = document.querySelector("#emailError");
-const emailErrorNotValid = document.querySelector("#emailErrorNotValid")
+const emailErrorNotValid = document.querySelector("#emailErrorNotValid");
+
+const password = document.querySelector("#password");
+const passwordError = document.querySelector("#passwordError");
+
+const confirmPassword = document.querySelector("#confirm_password");
+const confirmPasswordError = document.querySelector("#confirmPasswordError");
+
+const confirmPasswordErrorNotMatching = document.querySelector("#confirmPasswordErrorNotMatching");
+const generalErrorMessage = document.querySelector("#general-error-message");
 
 
-
-
-console.log(signUpForm, firstName, firstNameError, email, emailError, emailErrorNotValid);
+console.log(signUpForm, firstName, firstNameError, email, emailError, emailErrorNotValid, passwordError, password, confirmPasswordErrorNotMatching, confirmPassword, confirmPasswordError, generalErrorMessage);
 
 
