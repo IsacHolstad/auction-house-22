e2d81d40c72bf2658b2c03abf3c10c5376a868ef
isac holstad <wisac22@gmail.com>
2022-11-30 23:16:02 +0100

    signup and login is working

--- auction-house/js/settings/api.js
+++ auction-house/js/settings/api.js
@@ -1,12 +1,12 @@
 const API_BASE_URL = "https://nf-api.onrender.com/"
 
-const USER_REGISTER_URL = API_BASE_URL + "/api/v1/auction/auth/register"
-const USER_LOGIN = API_BASE_URL + "/api/v1/auction/auth/login"
-const AUCTION_LISTINGS = API_BASE_URL + "/api/v1/auction/listings"
-const POST_AUCTION = API_BASE_URL + "/api/v1/auction/listings"
+const USER_REGISTER_URL = API_BASE_URL + "api/v1/auction/auth/register"
+const USER_LOGIN = API_BASE_URL + "api/v1/auction/auth/login"
+const AUCTION_LISTINGS = API_BASE_URL + "api/v1/auction/listings"
+const POST_AUCTION = API_BASE_URL + "api/v1/auction/listings"
 
 
-//TODO ENDPOINTS IS NOT CORRECT
+//TODO ENDPOINTS IS CORRECT!!!
 
 
 // TODO  update auction = "/api/v1/auction/listings/<id>"
