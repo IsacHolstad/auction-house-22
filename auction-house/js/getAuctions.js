05ff19d3216d6f6defaff597141d3812cfc89801
isac holstad <wisac22@gmail.com>
2022-12-01 21:52:48 +0100

    all elements needed to show auction listings

--- auction-house/js/getAuctions.js
+++ auction-house/js/getAuctions.js
@@ -1,5 +1,8 @@
 import moment from "moment";
 import {getToken} from "./utils/storage.js";
+import {AUCTION_LISTINGS} from "./settings/api.js";
+
+console.log(AUCTION_LISTINGS)
 
 const accessToken = getToken()
 
@@ -7,3 +10,6 @@ if (!accessToken) {
     location.href = "/login.html"
 }
 const auctionImg = document.querySelector("#auction-img")
+const auctionTitle = document.querySelector("#auction-title")
+const auctionDescription = document.querySelector("#auction-description")
+const auctionPricing = document.querySelector("#auction-pricing")
