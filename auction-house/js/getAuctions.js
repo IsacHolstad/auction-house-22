a15f04460b602b8c7f7171ef54671351b75438f8
isac holstad <wisac22@gmail.com>
2022-12-01 21:30:42 +0100

    cannot see any sites if not logged in

--- auction-house/js/getAuctions.js
+++ auction-house/js/getAuctions.js
@@ -1,1 +1,9 @@
+import moment from "moment";
+import {getToken} from "./utils/storage.js";
+
+const accessToken = getToken()
+
+if (!accessToken) {
+    location.href = "/login.html"
+}
 const auctionImg = document.querySelector("#auction-img")
