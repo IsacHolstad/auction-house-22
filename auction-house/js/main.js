dab7bedb639a7fc09a18d2612c8f33cb722e049a
isac holstad <wisac22@gmail.com>
2022-12-06 10:23:03 +0100

    navigation is working

--- auction-house/js/main.js
+++ auction-house/js/main.js
@@ -1,3 +1,15 @@
 import '../style.css'
+import {clearStorage} from "./utils/storage.js";
+import creatNavigation from "./components/createNavigation.js"
 
+creatNavigation();
+
+const logOutbtn = document.querySelector("#logout-button");
+
+if (logOutbtn) {
+    logOutbtn.addEventListener("click", function () {
+        clearStorage();
+        window.location.replace("/login.html")
+    })
+}
 
