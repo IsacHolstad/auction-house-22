07dfefa09e45123aa4515a5b585b7d68edd4b774
isac holstad <wisac22@gmail.com>
2022-12-06 15:37:39 +0100

    details on spesific auction data info

--- auction-house/js/spesific-auction.js
+++ auction-house/js/spesific-auction.js
@@ -16,7 +16,7 @@ async function getAuctionById() {
         }
     })
     const data = await  response.json()
-    const {title, description, tags, media, created, updated, endsAt, seller, name, email, avatar, id} = data;
+    const {title, description, tags, media, created, updated, endsAt, seller, name, email, avatar, id, count, amount} = data;
     spesificAuctionItem.innerHTML = `<div class="mt-8">
                 <div class="border border-black-700 shadow rounded-xl p-4 max-w-2xl w-full mx-auto h-96">
                   <div class=" flex space-x-4">
@@ -25,8 +25,14 @@ async function getAuctionById() {
                       <div class="text-xl " id="auction-title">${title}</div>
                       <div class="space-y-3">
                         <div class="grid grid-cols-3 gap-4">
-                          <div class="h-2 bg-slate-300 rounded col-span-2" id="auction-pricing"></div>
+                          <div class="h-2  col-span-2" id="auction-pricing">Price: ${amount}</div>
                           <div class="h-10 text-center py-2  text-white bg-blue-400 rounded col-span-1">BID</div>
+                        </div>
+                        <div class="flex gap-4">
+                            <div>seller: ${seller}</div>
+                            <div>bids: ${count}</div>
+                            
+                        
                         </div>
                         <div class="flex text-xl py-4">
                             <div class="h-2  " id="auction-description">Posted: ${created}</div>
