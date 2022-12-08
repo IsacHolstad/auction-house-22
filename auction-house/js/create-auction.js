3575c1535cc1526506b1d6468f7da44549889085
isac holstad <wisac22@gmail.com>
2022-12-08 14:33:54 +0100

    all inputs for auction making added

--- auction-house/js/create-auction.js
+++ auction-house/js/create-auction.js
@@ -1,2 +1,10 @@
 import {getToken} from "./utils/storage.js";
-import {}
+import {CREATE_AUCTION} from "./settings/api.js";
+
+const createAuctionForm = document.querySelector("#create-auction-form")
+const auctionTitle = document.querySelector("#auctionTitle")
+const titleError = document.querySelector("#titleError")
+const auctionDescription = document.querySelector("#auctionDescription")
+const descriptionError = document.querySelector("#descriptionError")
+const auctionImg = document.querySelector("#auctionImg")
+const generalErrorMessage = document.querySelector("#general-error-message")
