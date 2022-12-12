3046d274ff24060a6e17fbf8d8a5e6ee50d257fb
isac holstad <wisac22@gmail.com>
2022-12-12 12:12:01 +0100

    making create post form

--- auction-house/js/create-auction.js
+++ auction-house/js/create-auction.js
@@ -1,11 +1,28 @@
 import {getToken} from "./utils/storage.js";
 import {CREATE_AUCTION, AUCTION_LISTINGS} from "./settings/api.js";
 
-const createAuctionForm = document.querySelector("#create-auction-form")
-const auctionTitle = document.querySelector("#auctionTitle")
-const titleError = document.querySelector("#titleError")
-const auctionDescription = document.querySelector("#auctionDescription")
-const descriptionError = document.querySelector("#descriptionError")
-const auctionImg = document.querySelector("#auctionImg")
-const generalErrorMessage = document.querySelector("#general-error-message")
+const createListingForm = document.querySelector("#create-listing-form");
 
+const listingTitle = document.querySelector("#listingTitle");
+const listDescription = document.querySelector("#listDescription");
+const listTagOne = document.querySelector("#listTagOne");
+const listTagTwo = document.querySelector("#listTagTwo");
+const listTagThree = document.querySelector("#listTagThree");
+const listImgOne = document.querySelector("#listImgOne");
+const listingEndDate = document.querySelector("#listingEndDate");
+
+createListingForm.addEventListener("submit", function (event) {
+    event.preventDefault()
+    console.log("i clicked the create button")
+
+    const listingTags = [listTagOne.value, listTagTwo.value, listTagThree.value];
+    const listingImg = [listImgOne.value];
+
+    const listingData = {
+        "title": listingTitle.value.trim(),
+        ""
+    }
+
+
+
+})
